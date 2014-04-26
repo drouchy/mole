d67926e64dddbf9cfd63848c95222970af537c60
David Rouchy <drouchy@gmail.com>
2014-04-26 11:15:05 +0100

    configure the mix

---
+++ lib/mole/cli.ex
@@ -0,0 +1,5 @@
+defmodule Mole.Cli do
+  def main(args) do
+    IO.puts "Hello World!\nMy name is mole"
+  end
+end
--- mix.exs
+++ mix.exs
@@ -5,23 +5,33 @@ defmodule Mole.Mixfile do
     [app: :mole,
      version: "0.0.1",
      elixir: "~> 0.13.0",
-     deps: deps]
+     escript_main_module: Mole.Cli,
+     escript_path: "_build/mole",
+     elixirc_options: options(Mix.env),
+     deps: deps(Mix.env)]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
-  def application do
-    [ applications: [],
-      mod: { Mole, [] } ]
-  end
-
-  # List all dependencies in the format:
-  #
-  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
-  # Type `mix help deps` for more examples and options
-  defp deps do
+  def options(_) do
     []
+  end
+
+  def application do
+    [
+      applications: [],
+      mod: { Mole, [] }
+    ]
+  end
+
+  defp deps(:test) do
+    [
+      { :mock,    github: "jjh42/mock"   },
+      { :webtest, github: "d0rc/webtest" }
+    ] ++ deps(:default)
+  end
+
+  defp deps(_) do
+    [
+      { :jsex,    github: "talentdeficit/jsex" }
+    ]
   end
 end
