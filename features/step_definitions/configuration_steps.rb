693d908c9bd78187fde7096df224665fa299bb2f
David Rouchy <drouchy@gmail.com>
2014-06-22 09:14:45 +0100

    fix the cukes

--- features/step_definitions/configuration_steps.rb
+++ features/step_definitions/configuration_steps.rb
@@ -2,5 +2,6 @@ require 'fileutils'
 
 Given(/^I have a regular config file$/) do
   config_file = File.expand_path(File.dirname(__FILE__)+ '/../../test/fixtures/config/regular.json')
-  FileUtils.cp(config_file, "#{ENV['HOME']}/.mole.json")
+  FileUtils.mkdir_p("#{ENV['HOME']}/.mole")
+  FileUtils.cp(config_file, "#{ENV['HOME']}/.mole/config.json")
 end
